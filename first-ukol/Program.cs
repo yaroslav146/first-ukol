462cf829fec8ef20c27478b0eb2c542dd51752b6
i24lovkya <i24lovkya@LAB550-04>
2026-09-03 13:49:19 +0200

    Přidejte soubory projektu.

---
+++ first-ukol/Program.cs
@@ -0,0 +1,22 @@
+﻿void menu()
+{
+    Console.WriteLine("Vítejte na programu nepláceneho, hládoveho IT-aka!");
+    Console.WriteLine("Váše volby:");
+    Console.WriteLine("-- 1 -- KALKULACKA");
+    Console.WriteLine("-- 2 -- RETEZCE");
+    Console.WriteLine("-- 3 -- KONEC - *konec programu");
+}
+
+while (true)
+{
+    menu();
+    int volba;
+    Console.Write("Zádejte volbu: ");
+    
+}
+
+
+
+
+
+
